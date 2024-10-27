de14fae2eb906e5244b4b248ae5781a420609c42
salahkh98 <salahyaseen484@gmail.com>
2024-10-27 22:04:43 +0200

    update requirements file2

--- db_handler.py
+++ db_handler.py
@@ -7,16 +7,18 @@ import os
 load_dotenv()
 
 # Fetch database credentials from environment variables
-DB_USER = os.getenv("POSTGRES_USER")
-DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-DB_HOST = os.getenv("POSTGRES_HOST", "localhost")  # Default to localhost
-DB_PORT = os.getenv("POSTGRES_PORT", "5432")       # Default to 5432
-DB_NAME = os.getenv("POSTGRES_DB")
+# DB_USER = os.getenv("POSTGRES_USER")
+# DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+# DB_HOST = os.getenv("POSTGRES_HOST", "localhost")  # Default to localhost
+# DB_PORT = os.getenv("POSTGRES_PORT", "5432")       # Default to 5432
+# DB_NAME = os.getenv("POSTGRES_DB")
 
 
 
 # Database URL should ideally be stored in environment variables for security.
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Create a global engine that will be used across the app
 engine = create_engine(DATABASE_URL)
